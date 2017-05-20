042258041a368918fa30956cd69550d2c6198562
M Starch <lestarch@googlemail.com>
2017-05-19 22:16:01 -0700

    lestarch: better listing (sorted)

--- src/repairs/repair-list.js
+++ src/repairs/repair-list.js
@@ -34,26 +34,47 @@ export class RepairList {
     }
     refresh() {
         RepairAPI.getRepairList().then(repairs => {
+            //Sort repairs
+            function compare(a,b) {
+                return a.id.localeCompare(b.id);
+            }
+            repairs.sort(compare);
+            //Update the array with newest entries
             for (var i = 0; i < repairs.length; i++) {
-                this.updateSingleListing(repairs[i]);
+                var newer = repairs[i];
+                var older = (i < this.repairs.length) ? this.repairs[i] : null;
+                //Remove old entries that don't exist any longer
+                while (older != null && older.id < newer.id) {
+                    this.repairs.splice(i,1);
+                    older = (i < this.repairs.length) ? this.repairs[i] : null;
+                }
+                //Get comparible string versions
+                var newer_str = JSON.stringify(newer.marshall());
+                var older_str = (older == null)?"":JSON.stringify(older.marshall());
+                //If identical skip
+                if (older != null && newer.id == older.id && newer_str == older_str) {
+                    continue;
+                }
+                //If updated, splice in
+                else if (older != null && newer.id == older.id && newer_str != older_str) {
+                    this.repairs.splice(i,1,repair);
+                    continue;
+                }
+                //Insert a new entry
+                else if (older == null || newer.id < older.id) {
+                    this.repairs.splice(i,0,newer);
+                    continue;
+                }
+                //Should never happen
+                else {
+                    throw Exception("[ERROR] Algorithm in bad state");
+                }
+            }
+            //Clean up extras
+            if (i < this.repairs.length) {
+                this.repairs.splice(i, this.repairs.length - i);
             }
         });
-    }
-    updateSingleListing(repair) {
-        var found = false;
-        for (var i = 0; i < this.repairs.length; i++) {
-            found = found || this.repairs[i].id == repair.id;
-            var existing = JSON.stringify(this.repairs[i].marshall());
-            var newer = JSON.stringify(repair.marshall());
-            if (this.repairs[i].id == repair.id && existing != newer) {
-                this.repairs.splice(i,1,repair);
-                found = true;
-            }
-        }
-        if (!found) {
-            this.repairs.push(repair);
-        }
-
     }
     select(repair) {
         this.selectedId = repair.id;
