0122d4c8378e51caf06990b5f7a14df0cfdf7669
M Starch <lestarch@googlemail.com>
2017-05-27 17:36:27 -0700

    lestarch: better utility scripts

--- bin/build-deploy.sh
+++ bin/build-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+WEBDIR="/var/www/html/"
 DIR=`dirname $0`
 au build --env prod
-scp ${DIR}/../scripts/* 192.168.0.2:/var/www/html/scripts/
-scp -r ${DIR}/../img/ 192.168.0.2:/var/www/html/
+cp -r ${DIR}/../index.html ${DIR}/../scripts/ ${DIR}/../img/ ${WEBDIR} 
