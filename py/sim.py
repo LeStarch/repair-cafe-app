7821f50716c98b63988cd97d6ba803a064f64e85
M Starch <lestarch@googlemail.com>
2018-05-26 10:42:09 -0700

    lestarch: fixed sim changes

--- py/sim.py
+++ py/sim.py
@@ -1,6 +1,7 @@
 import os
 import serial
 import flask
+import json
 
 DEV_DIR="/dev"
 DEV_PATTERN="ttyACM"
@@ -9,21 +10,25 @@ DEV_BAUD=19200
 ar_serial = None
 app = flask.Flask(__name__)
 
-@app.route("/phone", methods=["POST"])
+@app.route("/telephone", methods=["POST"])
 def send_text():
     '''
     Sends a text with the specified message to the specified number
     @param request.form["number"]: number to text
     @param request.form["message"]: message to send
     '''
-    number = request.form["number"]
-    message = request.form["message"]
+    tmp = flask.request.data.decode("utf-8") 
+    print("DATA:",tmp)
+    data = json.loads(tmp)
+    number = data["telephone"]
+    message = data["message"]
     number = number.replace("-", "").replace("+", "")
-    ar_serial.write(b"AT+CMGS=\"{0}\"\r\n".format(number))
-    ar_serial.write(b"{0}".format(message));
-    ar_serial.write(chr(0x1A))
+    ar_serial.write(bytes("AT+CMGS=\"{0}\"\r\n".format(number),"ascii"))
+    ar_serial.write(bytes("{0}".format(message),"ascii"));
+    ar_serial.write(bytes(chr(0x1A), 'ascii'))
     print("[INFO] Received message: '{0}'".format(
-          "\n".join(serial.readlines())))
+          "\n".join([ it.decode("ascii") for it in ar_serial.readlines()])))
+    return "GOOD"
 def setup(serial):
     '''
     Sets up the SIM device on the serial port
@@ -33,12 +38,12 @@ def setup(serial):
     serial.write(b"AT+IPR=19200\r\n")
     serial.write(b"AT+CMGF=1\r\n")
     print("[INFO] Received message: '{0}'".format(
-          "\n".join(serial.readlines())))
+          "\n".join([ it.decode("ascii") for it in serial.readlines()])))
 def main():
     '''
     Main program. Hi Lewis!!!!
     '''
-    global ar_port
+    global ar_serial
     print("[INFO] Detecting ports")
     port = None
     for item in os.listdir(DEV_DIR):
