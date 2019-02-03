3e55bc978f55c4b563810d8ece205da698df1a5f
M Starch <lestarch@googlemail.com>
2019-02-02 19:32:18 -0800

    lestarch: removing old https for http

--- bin/client/run-chromium.sh
+++ bin/client/run-chromium.sh
@@ -4,4 +4,4 @@ unclutter -idle 0.1 -root &
 xset s noblank
 xset s off
 xset -dpms
-chromium-browser --kiosk --ignore-certificate-errors --incognito https://rcuser:<PASSWD>@192.168.0.12/#/repairs/summary 
+chromium-browser --kiosk --ignore-certificate-errors --incognito http://rc-app/#/repairs/summary 
