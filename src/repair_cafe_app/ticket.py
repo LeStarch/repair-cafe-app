2efe9616a16b83264990a2da49cdbb40a5c1676b
mescoops <mescoops@gmail.com>
2024-01-22 23:52:14 -0800

    Ticket Layout +Logo

--- src/repair_cafe_app/ticket.py
+++ src/repair_cafe_app/ticket.py
@@ -10,14 +10,18 @@ import time
 from escpos import feature
 from escpos.impl.epson import GenericESCPOS
 from escpos import BluetoothConnection
+from PIL import Image, ImageOps
+import six
+
 
 class TicketPrinter(object):
-    
+   
     def __init__(self, settings):
         self.btConn = None
         self.printer = None
         self.connOK = False
         self.settings = settings
+        self.logoInit()
 
 
     def printTicket(self, queue, tNumber, owner, item, problem):
@@ -25,11 +29,12 @@ class TicketPrinter(object):
         self.printer.init()
         self.printer.text('-' * self.printer.feature.columns.normal)
         self.printer.text('-' * self.printer.feature.columns.normal)
+        self.logo()
         self.queue(queue)
         self.number(tNumber)
         self.owner(owner)
         self.item(item, problem)
-        self.logo()
+        self.logoName()
         self.location(self.settings.locationName(), self.settings.locationDate())
         self.stub(queue, tNumber, owner, item)
         self.endDoc()
@@ -59,9 +64,7 @@ class TicketPrinter(object):
         self.connOK = False
                   
            
-    def logo(self):
-        #printer.image("Logo_Repair_Circle_Grey.png")
-        #printer.device.write(<bytes>)
+    def logoName(self):
         self.printer.init()
         self.printer.justify_center()
         self.printer.set_expanded(True)
@@ -88,7 +91,7 @@ class TicketPrinter(object):
         self.printer.init()
         self.printer.justify_center()
         self.printer.set_text_size(3, 7)
-        self.printer.lf()
+        #self.printer.lf()
         self.printer.text(rqueue)
         self.printer.lf()
         time.sleep(0.1)
@@ -139,11 +142,13 @@ class TicketPrinter(object):
         self.printer.lf()
         self.printer.lf()
         self.printer.text('-' * self.printer.feature.columns.normal)
+        self.printer.justify_center()
+        self.printer.text("Patron Copy")
+        self.printer.lf()
         self.queue(queue)
         self.number(tNumber)
         self.owner(owner)
         self.item(item)
-        self.printer.lf()
 
         
     def location(self, loc: str, day: str):
@@ -156,5 +161,81 @@ class TicketPrinter(object):
         
     def endDoc(self):
         self.printer.lf()
+ 
+
+
+    def logoInit(self):   
+        filename = u"RepairCafe_White.png"
+        # Load Image
+        im = Image.open(filename)
+
+        self.high_density_horizontal = True
+        self.high_density_vertical = True
+        
+        # Initial rotate. mirror, and extract blobs for each 8 or 24-pixel row
+        # Convert to black & white via greyscale (so that bits can be inverted)
+        im = im.convert("L")  # Invert: Only works on 'L' images
+        im = ImageOps.invert(im) # Bits are sent with 0 = white, 1 = black in ESC/POS
+        im = im.convert("1") # Pure black and white
+        im = im.transpose(Image.ROTATE_270).transpose(Image.FLIP_LEFT_RIGHT)
+        self.imSize = im.size
+        line_height = 3 if self.high_density_vertical else 1
+        self.blobs = _to_column_format (im, line_height * 8);
+
+
+    def logo(self):
+        
+        # Generate ESC/POS header and print image
+        ESC = b"\x1b";
+        # Height and width refer to output size here, image is rotated in memory so coordinates are swapped
+        height_pixels, width_pixels = self.imSize
+        density_byte = (1 if self.high_density_horizontal else 0) + (32 if self.high_density_vertical else 0);
+        header = ESC + b"*" + six.int2byte(density_byte) + _int_low_high( width_pixels, 2 );
+        
+        self.printer.device.write(ESC + b"3" + six.int2byte(16)); # Adjust line-feed size
+        for blob in self.blobs:
+            self.printer.device.write(header + blob + b"\n")
+            time.sleep(0.01)
+        self.printer.device.write(ESC + b"2"); # Reset line-feed size
         self.printer.lf()
+
+
+
+
+def _to_column_format(im, line_height):
+    """
+    Extract slices of an image as equal-sized blobs of column-format data.
+
+    :param im: Image to extract from
+    :param line_height: Printed line height in dots
+    """
+    width_pixels, height_pixels = im.size
+    top = 0
+    left = 0
+    blobs = []
+    while left < width_pixels:
+        remaining_pixels = width_pixels - left
+        box = (left, top, left + line_height, top + height_pixels)
+        slice = im.transform((line_height, height_pixels), Image.EXTENT, box)
+        bytes = slice.tobytes()
+        blobs.append(bytes)
+        left += line_height
+    return blobs
+
+def _int_low_high(inp_number, out_bytes):
+    """ Generate multiple bytes for a number: In lower and higher parts, or more parts as needed.
+    
+    :param inp_number: Input number
+    :param out_bytes: The number of bytes to output (1 - 4).
+    """
+    max_input = (256 << (out_bytes * 8) - 1);
+    if not 1 <= out_bytes <= 4:
+        raise ValueError("Can only output 1-4 byes")
+    if not 0 <= inp_number <= max_input:
+        raise ValueError("Number too large. Can only output up to {0} in {1} byes".format(max_input, out_bytes))
+    outp = b'';
+    for _ in range(0, out_bytes):
+        outp += six.int2byte(inp_number % 256)
+        inp_number = inp_number // 256
+    return outp
 
