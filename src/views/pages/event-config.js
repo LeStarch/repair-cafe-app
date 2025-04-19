380e7e955c6a71397288d20acab85af3fd64eacc
M Starch <LeStarch@googlemail.com>
2025-04-19 09:06:04 -0700

    Double-secret time

--- src/views/pages/event-config.js
+++ src/views/pages/event-config.js
@@ -5,31 +5,43 @@ export let COMPONENT = {
     template: TEMPLATE,
     data: function () {
         return {
+            "server_count": 1,
             "time_error": null,
+            "remaining": 0,
         };
     },
     methods: {
         setTime: function () {
+            this.time_error = null;
             // Get the current time in seconds since the epoch
             let eventTime = Math.round(Date.now() / 1000.0);
             // Disable the button while the request is being processed
             let button = document.getElementById("set-time");
             button.disabled = true;
             // Set time on the backing server
-            console.log("Event time set to: " + eventTime);
-            WebApi.ajax("/set-time", "POST", null, null, {
-                "time": eventTime
-            }).then((response) => {
-                console.log("Server response: " + response);
-                // Re-enable the button after the request is processed
-                button.disabled = false;
-                this.time_error = null;
-            }).catch((error) => {
-                console.error("Error setting time: " + error);
-                // Re-enable the button after the request is processed
-                button.disabled = false;
-                this.time_error = error.error || "Unknown error";
-            });
+            console.log("Setting event time to: " + eventTime);
+            this.remaining = this.server_count;
+            for (let i = 0; i < this.server_count; i++) {
+                WebApi.ajax("/set-time", "POST", null, null, {
+                    "time": eventTime
+                }).then((response) => {
+                    this.remaining = this.remaining - 1;
+                    console.log("Set times remaining: " + this.remaining);
+                    // Check if this is the last response before re-enabling the button
+                    if (this.remaining == 0) {
+                        button.disabled = false;
+                    }
+                }).catch((error) => {
+                    this.remaining = this.remaining - 1;
+                    console.error("Error setting time: " + (error.error || "unknown") +
+                                  " remaining: " + this.remaining);
+                    this.time_error = error.error || "Unknown error";
+                    // Check if this is the last response before re-enabling the button
+                    if (this.remaining == 0) {
+                        button.disabled = false;
+                    }
+                });
+            }
         }
     },
 };
--- src/views/pages/event-config.template.js
+++ src/views/pages/event-config.template.js
@@ -1,15 +1,26 @@
 export let TEMPLATE=`
-<div class="row" >
-    <div class="col col-lg-4">
-        <label for="set-time">Set Event Time (From This Device)</label>
+<div class="form-group">
+    <div class="row">
+        <div class="col col-lg-4">
+            <label for="server-count">Server Count</label>
+        </div>
+        <div class="col col-lg-4">
+            <input type="number" id="server-count" class="form-control"
+                v-model="server_count" min="1" max="10" step="1" />
+        </div>
     </div>
-    <div class="col col-lg-4">
-        <input type="button" class="btn btn-block btn-success" id="set-time"
-               value="Set Time" @click="setTime" />
-    </div>
-    <div class="col col-lg-4 alert alert-danger" v-show="time_error != null">
-        <strong>Error Setting Time: </strong>
-        <span class="text-danger">{{ time_error }}</span>
+    <div class="row" >
+        <div class="col col-lg-4">
+            <label for="set-time">Set Event Time (From This Device)</label>
+        </div>
+        <div class="col col-lg-4">
+            <input type="button" class="btn btn-block btn-success" id="set-time"
+                value="Set Time" @click="setTime" />
+        </div>
+        <div class="col col-lg-4 alert alert-danger" v-show="time_error != null">
+            <strong>Error Setting Time: </strong>
+            <span class="text-danger">{{ time_error }}</span>
+        </div>
     </div>
 </div>
 `;
